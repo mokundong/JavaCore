fb4cd05069a9de1bd0ff85cc2365c24064253ebb
mokundong <mokundong@sina.com>
2017-10-28 13:04:28 +0800

    mkd

---
+++ src/ch03/StaticTest/Employee.java
@@ -0,0 +1,44 @@
+package ch03.StaticTest;
+
+import java.time.LocalDate;
+
+/**
+ * Created by MKD on 2017/10/28
+ */
+public class Employee {
+    private static int nextId = 1;
+
+    private  String name;
+    private double salary;
+    private int id;
+
+    //constructor        构造器 与类同名
+    public Employee(String n,double s){
+        name = n;
+        salary = s;
+    }
+
+    //method             方法
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId() {
+        id = nextId;
+        nextId++;
+    }
+    public static int getNextId(){
+        return nextId;
+    }
+
+    public static void main(String[] args) {
+        Employee e = new Employee("Harry",50000);
+        System.out.println(e.getName() + " " + e.getSalary());
+    }
+}
---
+++ src/ch03/employee/Employee.java
@@ -0,0 +1,47 @@
+package ch03.employee;
+
+import java.time.LocalDate;
+
+class Employee {
+    //instance field     实例域
+    private static int nextId = 1;
+    private int id;
+    private final String name;
+    private double salary;
+    private LocalDate hireday;
+
+    //constructor        构造器 与类同名
+    public Employee(String n,double s,int year,int month,int day){
+        name = n;
+        salary = s;
+        hireday = LocalDate.of(year,month,day);
+    }
+    //method             方法
+
+    public void setId(int id) {
+        id = nextId;
+        nextId++;
+    }
+    public static int getNextId(){
+        return nextId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireday() {
+        return hireday;
+    }
+    public void raiseSalary(double byPrecent){
+        double raise = this.salary * byPrecent / 100;
+        this.salary += raise;
+    }
+    public boolean equals(Employee other){
+        return name.equals(other.name);
+    }
+}
