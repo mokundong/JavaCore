8ecd20f86bf940015e797a37ad0f89351bb53dca
mokundong <mokundong@sina.com>
2017-11-15 21:09:32 +0800

    mkd

--- src/core2/TeseDemo.java
+++ src/core2/TeseDemo.java
@@ -1,7 +1,21 @@
 package core2;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 /**
  * Created by MKD on 2017/11/13
  */
 public class TeseDemo {
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintWriter out = new PrintWriter("a.txt");
+        String name = "name";
+        double sa = 500;
+        out.print(name);
+        out.print(' ');
+        out.print(sa);
+        out.close();
+    }
 }
