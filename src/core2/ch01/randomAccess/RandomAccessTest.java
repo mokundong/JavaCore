cf165f5971e483de0ece051a18e754f3bc2d9e8e
mokundong <mokundong@sina.com>
2017-11-17 23:10:55 +0800

    mkd

--- src/core2/ch01/randomAccess/RandomAccessTest.java
+++ src/core2/ch01/randomAccess/RandomAccessTest.java
@@ -1,8 +1,8 @@
 package core2.ch01.randomAccess;
 
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Created by MKD on 2017/11/16
@@ -18,8 +18,52 @@ public class RandomAccessTest {
         try (DataOutputStream out = new DataOutputStream(new FileOutputStream("employee.dat"))){
             //save all records into a new array
             for (Employee e : staff){
-
+                writeData(out,e);
             }
         }
+        try (RandomAccessFile in = new RandomAccessFile("employee.dat","r")){
+            //retrieve all records into a new array
+            //compute the array size
+            int n = (int) (in.length() / Employee.RECORD_SIZE);
+            Employee[] newStaff = new Employee[n];
+            //read employees in reverse order
+            for (int i=n-1;i>=0;i--){
+                newStaff[i] = new Employee();
+                in.seek(i * Employee.RECORD_SIZE);
+                newStaff[i] = readData(in);
+            }
+            //print the newly read employee records
+            for (Employee e : newStaff)
+                System.out.println(e);
+        }
     }
+    /**
+     * writes employee data to a data output
+     * @param out the data out
+     * @param e the employee
+     */
+    public static void writeData(DataOutput out,Employee e) throws IOException{
+        DataIO.writeFixedString(e.getName(),Employee.NAME_SIZE,out);
+        out.writeDouble(e.getSalary());
+
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTime(e.getHireDay());
+        out.writeInt(calendar.get(Calendar.YEAR));
+        out.writeInt(calendar.get(Calendar.MONTH));
+        out.writeInt(calendar.get(Calendar.DAY_OF_MONTH));
+    }
+    /**
+     * read employee data from a data input
+     * @param in the data  input
+     * @return the employee
+     */
+    public static Employee readData(DataInput in) throws IOException{
+        String name = DataIO.readFixedString(Employee.NAME_SIZE,in);
+        double salary = in.readDouble();
+        int y = in.readInt();
+        int m = in.readInt();
+        int d = in.readInt();
+        return new Employee(name,salary,y,m-1,d);
+    }
+
 }
